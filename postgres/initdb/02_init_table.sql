cc9c49c34996941e8bac5805f7a17ad4363b7969
DaisukeHirabayashi <daisuke@pluslab.org>
2021-11-22 00:52:30 +0900

    dbのテーブル名の変更

--- postgres/initdb/02_init_table.sql
+++ postgres/initdb/02_init_table.sql
@@ -1,16 +1,16 @@
 \c go-pagenation;
 
-CREATE TABLE IF NOT EXISTS shop (
+CREATE TABLE IF NOT EXISTS shops (
     id SERIAL NOT NULL,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
-INSERT INTO "public"."shop" ("name") VALUES ('Pretty');
-INSERT INTO "public"."shop" ("name") VALUES ('Beauty-Beauty');
-INSERT INTO "public"."shop" ("name") VALUES ('Beauty-Beauty');
-INSERT INTO "public"."shop" ("name") VALUES ('Beauty-Beauty');
-INSERT INTO "public"."shop" ("name") VALUES ('Beauty-Beauty');
-INSERT INTO "public"."shop" ("name") VALUES ('Beauty-Beauty');
-INSERT INTO "public"."shop" ("name") VALUES ('Beauty-Beauty');
+INSERT INTO "public"."shops" ("name") VALUES ('Pretty');
+INSERT INTO "public"."shops" ("name") VALUES ('Beauty-Beauty');
+INSERT INTO "public"."shops" ("name") VALUES ('Beauty-Beauty');
+INSERT INTO "public"."shops" ("name") VALUES ('Beauty-Beauty');
+INSERT INTO "public"."shops" ("name") VALUES ('Beauty-Beauty');
+INSERT INTO "public"."shops" ("name") VALUES ('Beauty-Beauty');
+INSERT INTO "public"."shops" ("name") VALUES ('Beauty-Beauty');
